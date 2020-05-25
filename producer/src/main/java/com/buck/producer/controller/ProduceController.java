8cd382cfb47b8d8b77e620acda7d033ee28cea8c
Buck <790721085@qq.com>
2020-05-25 17:28:15 +0800

    no msg

--- producer/src/main/java/com/buck/producer/controller/ProduceController.java
+++ producer/src/main/java/com/buck/producer/controller/ProduceController.java
@@ -2,12 +2,16 @@ package com.buck.producer.controller;
 
 import com.buck.producer.bean.Properties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @Controller
 @RestController
@@ -17,10 +21,16 @@ public class ProduceController {
     @Autowired
     Properties props;
 
+
     @GetMapping("/{id}")
-    @HystrixCommand(fallbackMethod = "fallback")
+    @HystrixCommand(fallbackMethod = "fallback", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")})
     public String produce(@PathVariable("id")Integer id) {
-        int a = 10/0;
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return id.toString() +  "  "  + Thread.currentThread().getName() + "---" + props.toString();
     }
 
